e016b8e5f03babca6ec8790ce4ba920296b8f44d
Daniel <danny@buffered.me>
2020-10-29 17:17:10 +0100

    chore: remove obsolete meta

--- Sources/ReleaseTools/meta.swift
+++ Sources/ReleaseTools/meta.swift
@@ -1,7 +1,7 @@
     struct Metadata {
         let build = "124"
-        let version = "v1.1.1-26-g40d2e6e"
-        let commit = "40d2e6e0044f4ce8348d5a4f426cecdb32d61bc3"
+        let version = "v1.1.2"
+        let commit = "not-set"
         let tags = "heads/master"
 
         static var main = Metadata()
